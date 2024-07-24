c4b0edf1740b147182042dd444e6486fe62b4105
Pericles Barreto <132623133+pericles81@users.noreply.github.com>
2024-07-24 15:40:44 -0300

    Add files via upload

---
+++ env.php
@@ -0,0 +1,9 @@
+<?php
+
+putenv('DISPLAY_ERRORS_DETAILS=' . true);
+
+putenv('PROJETO_SERDUC_MYSQL_HOST=localhost');
+putenv('PROJETO_SERDUC_MYSQL_DBNAME=projeto_serduc');
+putenv('PROJETO_SERDUC_MYSQL_USER=root');
+putenv('PROJETO_SERDUC_MYSQL_PASSWORD=root');
+putenv('PROJETO_SERDUC_MYSQL_PORT=3306');
